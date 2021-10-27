e7dd62b11f73e4dea53b3c8270d49c352fde541d
Andrew Connor <andrewjamesconnor@gmail.com>
2021-10-27 22:27:12 +0100

    Check if logged in

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# Keep PC connected to BT Open WiFi
+import subprocess
+import requests
+
+# Detect if connected to bridge
+wifi_name = 'HUAWEI-4211-2.4G'
+
+if wifi_name in subprocess.check_output('netsh wlan show interfaces').decode('utf-8'):
+    # Connected
+    r = requests.get('https://www.btwifi.com:8443')
+    if 'logged on to BT Wi-Fi' in r.text:
+        print('Logged in')
+    else:
+        print('Not logged in')
