8093ee160198b6d931adb19375db16680fa2ee75
Andrew Connor <andrewjamesconnor@gmail.com>
2021-10-27 23:23:14 +0100

    Added hotkey

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
 import subprocess
 import requests
 import os
+from pynput.keyboard import Key, Listener, KeyCode
 
 # Detect if connected to bridge
-wifi_name = open(f'{os.path.expanduser("~")}/wifi.txt', 'r').read()
+wifi_name = open(f'{os.path.expanduser("~")}/wifi/name.txt', 'r').read()
 
-if wifi_name in subprocess.check_output("netsh wlan show interfaces").decode("utf-8"):
-    # Connected
+
+def wifi_on():
+    return wifi_name in subprocess.check_output("netsh wlan show interfaces").decode("utf-8")
+
+
+def reconnect():
+    print("Hotkey pressed")
+    # Send POST to log in
+    #if wifi_on():
+        #requests.post("https://www.btwifi.com:8443", ...)
+
+
+if wifi_on():  # Connected
     r = requests.get("https://www.btwifi.com:8443")
-    if "logged on to BT Wi-Fi" in r.text:
-        print("Logged in")
+    if "bthub-loginForm__toggleButton" in r.text:
+        print("Logged out")
+        reconnect()
     else:
-        print("Not logged in")
+        print("Logged in")
+
+# Have to wait to be logged out of WiFi to continue...
+# Implement hotkey to attempt reconnection on press
+
+combo = (Key.shift, KeyCode.from_char("¬"))
+current = set()
+
+
+def on_press(key):
+    if key in combo:
+        current.add(key)
+        if set(combo) == current:
+            reconnect()
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
