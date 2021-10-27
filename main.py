4115413c6e5e60b5358e21a911bcecb4ef226617
Andrew Connor <andrewjamesconnor@gmail.com>
2021-10-27 22:33:05 +0100

    Update WiFi name

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import subprocess
 import requests
+import os
 
 # Detect if connected to bridge
-wifi_name = 'HUAWEI-4211-2.4G'
+wifi_name = open(f'{os.path.expanduser("~")}/wifi.txt', 'r').read()
 
-if wifi_name in subprocess.check_output('netsh wlan show interfaces').decode('utf-8'):
+if wifi_name in subprocess.check_output("netsh wlan show interfaces").decode("utf-8"):
     # Connected
-    r = requests.get('https://www.btwifi.com:8443')
-    if 'logged on to BT Wi-Fi' in r.text:
-        print('Logged in')
+    r = requests.get("https://www.btwifi.com:8443")
+    if "logged on to BT Wi-Fi" in r.text:
+        print("Logged in")
     else:
-        print('Not logged in')
+        print("Not logged in")
